97cce001bcdfb562881435407d3ae1f9bee725e4
developmentdesignsltd <developmentdesignsltd@gmail.com>
2025-12-04 17:04:38 +0000

    c

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-apod-description.ts';
-import '@/ai/flows/generate-apod-explanation-eli5.ts';
-import '@/ai/flows/generate-image.ts';
